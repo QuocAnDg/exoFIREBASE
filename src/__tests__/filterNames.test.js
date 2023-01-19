e66e0d7b9e47bd369f465468ee60aac85ea457bf
QuocAnDg <quocand40@gmail.com>
2023-01-20 00:13:43 +0100

    feat: add first unit test

---
+++ src/__tests__/filterNames.test.js
@@ -0,0 +1,6 @@
+describe("app tests suites - filterNamesX", () => {
+    test("should return empty list", () => {
+        const result = getCurrentMonth();
+        expect(result).toBe("January");
+    });
+});
---
+++ src/filterNames.js
@@ -0,0 +1,3 @@
+const filterNamesX = (noms) => {
+    return [];
+};
